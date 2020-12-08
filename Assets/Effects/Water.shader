0e8cbbe4bda8b5581a2cf0dcc9178b1b68482f7a
Luca Mefisto <luca.m.conesa@gmail.com>
2020-12-08 11:31:40 +0100

    imported oculus and hand posing

---
+++ Assets/Effects/Water.shader
@@ -0,0 +1,52 @@
+﻿Shader "Custom/Water"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Bump ("Bump", 2D) = "white" {}
+        _BumpDetail ("Bump detail", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+        sampler2D _Bump;
+        sampler2D _BumpDetail;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float2 uv_BumpDetail;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+            o.Albedo = c.rgb;
+            
+            fixed4 normal = tex2D (_Bump, IN.uv_MainTex);
+            fixed4 detailNormal = tex2D (_BumpDetail, IN.uv_BumpDetail);
+            o.Normal = BlendNormals(normal, detailNormal);
+
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Standard (Specular)"
+}
---
+++ Assets/Samples/HandPosing/1.2.1/Integration Oculus (OVR)/Scripts/GrabberHybridOVR.cs
@@ -0,0 +1,76 @@
+﻿using HandPosing.Interaction;
+using System;
+using UnityEngine;
+
+namespace HandPosing.OVRIntegration
+{
+    public class GrabberHybridOVR : BaseGrabber
+    {
+        [Header("OVR dependencies")]
+        [SerializeField]
+        private OVRHand trackedHand;
+        [SerializeField]
+        private Vector2 grabThresoldController = new Vector2(0.35f, 0.85f);
+        [SerializeField]
+        private Vector2 grabThresoldHand = new Vector2(0f,0.95f);
+
+        [SerializeField]
+        private Handeness handeness;
+
+        private OVRInput.Controller touch;
+        private OVRInput.Controller hand;
+
+        protected override void Reset()
+        {
+            base.Reset();
+            if(name.ToLower().Contains("right"))
+            {
+                handeness = Handeness.Right;
+            }
+            else
+            {
+                handeness = Handeness.Left;
+            }
+        }
+
+        protected override void Awake()
+        {
+            touch = handeness == Handeness.Right ? OVRInput.Controller.RTouch : OVRInput.Controller.LTouch;
+            hand = handeness == Handeness.Right ? OVRInput.Controller.RHand : OVRInput.Controller.LHand;
+            base.Awake();
+        }
+
+        public override float CurrentFlex()
+        {
+            if (IsUsingHands)
+            {
+                return Math.Max(trackedHand.GetFingerPinchStrength(OVRHand.HandFinger.Index),
+                     trackedHand.GetFingerPinchStrength(OVRHand.HandFinger.Middle));
+            }
+            else
+            {
+                return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, touch);
+            }
+        }
+
+        public override Vector2 GrabFlexThresold
+        {
+            get
+            {
+                return IsUsingHands ? grabThresoldHand : grabThresoldController;
+            }
+        }
+
+        private bool IsUsingHands => trackedHand && trackedHand.IsTracked;
+
+        protected override (Vector3, Vector3) HandRelativeVelocity(Pose offsetPose)
+        {
+            OVRInput.Controller controller = IsUsingHands ? hand : touch;
+
+            Pose trackingSpace = Pose.identity;
+            Vector3 linearVelocity = trackingSpace.rotation * OVRInput.GetLocalControllerVelocity(controller);
+            Vector3 angularVelocity = trackingSpace.rotation * OVRInput.GetLocalControllerAngularVelocity(controller);
+            return (linearVelocity, angularVelocity);
+        }
+    }
+}
