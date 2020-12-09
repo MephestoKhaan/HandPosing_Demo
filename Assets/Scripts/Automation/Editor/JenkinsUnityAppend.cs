68bfca45f43e0d2964a8dd14a49a1b3f26d17a7c
Luca Mefisto <luca.m.conesa@gmail.com>
2020-12-09 17:12:31 +0100

    adding build system

---
+++ Assets/Scripts/Automation/Editor/JenkinsUnityAppend.cs
@@ -0,0 +1,69 @@
+﻿using UnityEditor;
+
+static class JenkinsUnityAppend
+{
+	
+	public static readonly string APP_ID_RELEASE = "com.mefistofiles.handposing_techdemo";
+
+
+	private static void AppendAndroidRelease()
+	{
+		AppendAndroid(
+			keystoreName: GetArg("-keystore"),
+			keystorepass: GetArg("-keystorepass"),
+			keyaliasName: GetArg("-keyalias"),
+			keyaliasPass: GetArg("-keyaliasPass"),
+			buildNumber: GetArg("-buildnumber"),
+			name: GetArg("-apkName"));
+	}
+
+	private static void AppendAndroid(string buildNumber,
+		string keystoreName,
+		string keystorepass,
+		string keyaliasName,
+		string keyaliasPass, 
+		string name)
+	{
+		PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
+
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        EditorUserBuildSettings.androidBuildType = AndroidBuildType.Release;
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = false;
+        EditorUserBuildSettings.development = false;
+		PlayerSettings.Android.useAPKExpansionFiles = false;
+		PlayerSettings.applicationIdentifier = APP_ID_RELEASE;
+
+		PlayerSettings.Android.keystoreName = keystoreName;
+        PlayerSettings.Android.keystorePass = keystorepass;
+        PlayerSettings.Android.keyaliasName = keyaliasName;
+        PlayerSettings.Android.keyaliasPass = keyaliasPass;
+		
+        BuildPipeline.BuildPlayer(GetScenePaths(), $"./{name}.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+
+	static string[] GetScenePaths()
+	{
+		string[] scenes = new string[EditorBuildSettings.scenes.Length];
+
+		for (int i = 0; i < scenes.Length; i++)
+		{
+			scenes[i] = EditorBuildSettings.scenes[i].path;
+		}
+
+		return scenes;
+	}
+
+	private static string GetArg(string name)
+	{
+		var args = System.Environment.GetCommandLineArgs();
+		for (int i = 0; i < args.Length; i++)
+		{
+			if (args[i] == name && args.Length > i + 1)
+			{
+				return args[i + 1];
+			}
+		}
+		return null;
+	}
+}
