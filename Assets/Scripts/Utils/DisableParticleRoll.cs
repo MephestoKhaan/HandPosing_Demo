6c4d787f686cf63e5155c587f605a447c493ed1c
Luca Mefisto <luca.m.conesa@gmail.com>
2020-12-26 14:25:04 +0100

    improved particle systems without roll and callback

---
+++ Assets/Scripts/Utils/DisableParticleRoll.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Unity.Burst;
+using UnityEngine.ParticleSystemJobs;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class DisableParticleRoll : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+    private Job _job = new Job();
+
+    private float _lastRotation;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        Camera camera = Camera.main;
+        if (camera == null)
+        {
+            return;
+        }
+
+        float currentRotation = camera.transform.rotation.eulerAngles.z;
+        _job.rotationDifference = (currentRotation - _lastRotation) * Mathf.Deg2Rad;
+        _lastRotation = currentRotation;
+    }
+
+    void OnParticleUpdateJobScheduled()
+    {
+        if (_job.rotationDifference != 0.0f)
+        {
+            _job.ScheduleBatch(_particleSystem, 4096);
+        }
+    }
+
+    [BurstCompile]
+    struct Job : IJobParticleSystemParallelForBatch
+    {
+        public float rotationDifference;
+
+        public void Execute(ParticleSystemJobData particles, int startIndex, int count)
+        {
+            var rotations = particles.rotations.z;
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                rotations[i] += rotationDifference;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/ParticleSystemCaller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSystemCaller: MonoBehaviour
+{
+    private ParticleSystem particles;
+
+    private System.Action stoppedCallback;
+
+    private void Awake()
+    {
+        particles = this.GetComponent<ParticleSystem>();
+    }
+
+    public void Play(System.Action onStopped)
+    {
+        stoppedCallback = onStopped;
+        particles.Play(true);
+    }
+
+    private void OnParticleSystemStopped()
+    {
+        stoppedCallback?.Invoke();
+    }
+}
