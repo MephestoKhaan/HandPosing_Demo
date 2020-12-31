4672999e26e2f4780b46260c45215a5bedd848e5
Luca Mefisto <luca.m.conesa@gmail.com>
2020-12-31 16:04:01 +0100

    utulity to set FFR

---
+++ Assets/Scripts/Utils/FixedFoveatedRendering.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FixedFoveatedRendering : MonoBehaviour
+{
+    void Start()
+    {
+        OVRPlugin.SystemHeadset headset = OVRPlugin.GetSystemHeadsetType();
+        if (headset == OVRPlugin.SystemHeadset.Oculus_Quest)
+        {
+            OVRManager.fixedFoveatedRenderingLevel = OVRManager.FixedFoveatedRenderingLevel.Medium;
+
+        }
+        else if (headset == (OVRPlugin.SystemHeadset.Oculus_Quest_2))
+        {
+            OVRManager.fixedFoveatedRenderingLevel = OVRManager.FixedFoveatedRenderingLevel.Off;
+
+        }
+    }
+}
